901aa811267af385e61c98d612b7ca7ab3e1de9c
Johan Ruokangas <johan@latehours.net>
2016-02-15 23:49:33 +0200

    Parallax poster image

--- app/components/Event.js
+++ app/components/Event.js
@@ -5,6 +5,8 @@ import React, {
   StyleSheet,
   TouchableOpacity,
   Image,
+  View,
+  Animated
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux';
@@ -14,22 +16,57 @@ import api from '../utils/api';
 
 export default class Event extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: new Animated.Value(0)
+    }
+  }
+
   render() {
     const { events } = this.props;
+    const { scrollY } = this.state;
     console.log(events);
 
+    const fakeTickets = Array(10).fill(events.tickets[0]);
+    events.tickets = fakeTickets;
+
     return (
-      <ScrollView style={styles.container}>
-        <Image
-          style={{ width: 320, height: 480/3 }}
+      <View style={styles.container}>
+        <Animated.Image
+          style={{
+            width: 320,
+            height: scrollY.interpolate({
+              /*inputRange: [0, 480/3],
+              outputRange: [480/3*.8, 0]*/
+              inputRange: [ -300, 0, 300],
+              outputRange: [300/2, 0, -300/3]
+            }),
+            backgroundColor: '#aaa',
+          }}
           source={{ uri: events.poster }}
         />
-        <Tickets
-          venue={events.venue}
-          tickets={events.tickets}
-          onPressTicket={this.handleOnPressTicket.bind(this)}
-        />
-      </ScrollView>
+        <Animated.View
+          style={{
+            flex: 1
+          }}
+        >
+        <ScrollView
+          style={styles.container}
+          onScroll={Animated.event([
+            { nativeEvent: { contentOffset: { y: this.state.scrollY }}}
+          ])}
+          scrollEventThrottle={16}
+        >
+          <Tickets
+            venue={events.venue}
+            tickets={events.tickets}
+            onPressTicket={this.handleOnPressTicket.bind(this)}
+          />
+        </ScrollView>
+      </Animated.View>
+      </View>
     );
   }
 
